0f537b4ad9cb8897894839b0adf6d24981e671ab
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2022-06-20 15:33:12 +0300

    fix

--- src/app/tasks/services/task-promise.service.ts
+++ src/app/tasks/services/task-promise.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 import type { TaskModel } from './../models/task.model';
-import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'any'
